7d70d972ed141ec1f98c78326c237e3beedfb1c9
amparab <amruta0303@gmail.com>
2024-03-01 01:27:52 +0530

    Created timeline : animation

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -15,7 +15,7 @@ export default function Experience() {
                     <ReactTyped strings={["My Education and Experience"]} typeSpeed={50} />
                 </div>
                 <div className="w-px bg-black relative left-1/3 flex items-center h-40">
-                    <div className="ml-6 p-4 rounded-md flex items-center bg-gradient-to-br from-black via-pink-950 to-pink-700" style={{ minWidth: '300px', width: 'fit-content' }}>
+                    <div className="section-1 ml-6 p-4 rounded-md flex items-center bg-gradient-to-br from-black via-pink-950 to-pink-700" style={{ minWidth: '300px', width: 'fit-content' }}>
                         <div className="w-24 h-24 mr-4 shadow-xl rounded-full overflow-hidden flex-shrink-0">
                             <img className="w-full h-full object-cover rounded-full" src={VesitLogo} alt="Vesit Logo" />
                         </div>
@@ -25,10 +25,10 @@ export default function Experience() {
                             <p className="font-pixel text-xs text-white">(2016 - 2020)</p>
                         </div>
                     </div>
-                    <div className="w-6 h-6 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for Vesit */}
+                    <div className="circle-1 w-3 h-3 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for Vesit */}
                 </div>
                 <div className="w-px bg-black relative left-1/3 flex items-center h-40">
-                    <div className="ml-6 p-4 rounded-md flex items-center bg-gradient-to-br from-black via-pink-950 to-pink-700" style={{ minWidth: '300px', width: 'fit-content' }}>
+                    <div className="section-2 ml-6 p-4 rounded-md flex items-center bg-gradient-to-br from-black via-pink-950 to-pink-700" style={{ minWidth: '300px', width: 'fit-content' }}>
                         <div className="w-24 h-24 mr-4 shadow-xl rounded-full overflow-hidden flex-shrink-0">
                             <img className="w-full h-full object-cover rounded-full" src={JPLogo} alt="JP Logo" />
                         </div>
@@ -38,33 +38,33 @@ export default function Experience() {
                             <p className="font-pixel text-xs text-white">(2016 - 2020)</p>
                         </div>
                     </div>
-                    <div className="w-6 h-6 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for JP */}
+                    <div className="circle-2 w-3 h-3 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for JP */}
                 </div>
                 <div className="w-px bg-black relative left-1/3 flex items-center h-32" >
-                    <div className="ml-6 flex items-center" style={{ minWidth: '300px', width: 'fit-content' }}>
+                    <div className="section-3 ml-6 flex items-center" style={{ minWidth: '300px', width: 'fit-content' }}>
                         <div className="flex flex-col flex-grow">
                             <div className="font-pixel text-black">Position 1</div>
                             <p className="font-pixel text-xs text-black">Job Description 1</p>
                             <p className="font-pixel text-xs text-black">(2016 - 2020)</p>
                         </div>
                     </div>
-                    <div className="w-6 h-6 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for Position 1 */}
+                    <div className="circle-3 w-3 h-3 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for Position 1 */}
                 </div>
                 <div className="w-px bg-black relative left-1/3 flex items-center h-32">
-                    <div className="ml-6 flex items-center" style={{ minWidth: '300px', width: 'fit-content' }}>
+                    <div className="section-4 ml-6 flex items-center" style={{ minWidth: '300px', width: 'fit-content' }}>
                         <div className="flex flex-col flex-grow">
                             <div className="font-pixel text-black">Position 2</div>
                             <p className="font-pixel text-xs text-black">Job Description 2</p>
                             <p className="font-pixel text-xs text-black">(2016 - 2020)</p>
                         </div>
                     </div>
-                    <div className="w-6 h-6 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for Position 2 */}
+                    <div className="circl-4 w-3 h-3 rounded-full bg-black absolute left-1/2 transform -translate-x-1/2"></div> {/* Circle for Position 2 */}
                 </div>
             </div>
         </div>
 
-        
 
-    
+
+
     );
 }
