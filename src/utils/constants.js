b175640127f6e99177a6f599df00b51cf7f021bd
amparab <amruta0303@gmail@gmail.com>
2024-04-12 22:53:32 +0530

    Renaming

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const skillTriggerDegrees = 120;
+
+export const minRotation = 180;
+
+export const large_screen_breakpoint = 768;
+
+export const svgMaskWidth = 350;
+export const svgMaskHeight = 350;
+export const initialAngle = 0;
+
